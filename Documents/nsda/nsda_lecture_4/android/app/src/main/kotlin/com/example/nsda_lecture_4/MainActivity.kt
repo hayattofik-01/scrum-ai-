89ed548033cf73cac39b3b3a9a9897fa61654f00
Hayat Tofik <129114795+hayattofik-01@users.noreply.github.com>
2025-12-26 22:15:37 +0100

    Delete Documents directory

--- Documents/nsda/nsda_lecture_4/android/app/src/main/kotlin/com/example/nsda_lecture_4/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.nsda_lecture_4
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- Documents/nsda/nsda_lecture_4/lib/presentation/deed_card/models/deed.dart
+++
@@ -1,6 +0,0 @@
-/// Simple data model for each deed item.
-class Deed {
-  final String title;
-  bool isDone;
-  Deed(this.title, {this.isDone = false});
-}
