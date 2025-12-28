551b2f44e05aef10a410d71a0a5ed31d0868526d
hayattofik-01 <hayattofik22@gmail.com>
2025-12-28 14:22:31 +0100

    move the variables to env

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://ccceiuyajfpyqagrsmsm.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNjY2VpdXlhamZweXFhZ3JzbXNtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY3NjkzNDAsImV4cCI6MjA4MjM0NTM0MH0.vAM_3z__OQPjhHzZ3d9ctwK_0cmK9mtlnwtAl_QLowo';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
